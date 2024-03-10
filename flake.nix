b4dceca31278dee17d1d4657f43c05ee8897e251
Fabio Keller <fabio.keller@exb.de>
2024-03-10 15:14:42 +0100

    fix(flake): devshell for macos

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   outputs = { self, nixpkgs }:
     let
       defaultSystem = "x86_64-linux";
+      macSystem = "aarch64-darwin";
       pkgs = import nixpkgs { system = defaultSystem; };
 
       markdownToHtmlCli = import ./nix/markdown2html-converter.nix { inherit pkgs; };
@@ -18,6 +19,9 @@
     in
     {
       devShells."${defaultSystem}".default = pkgs.mkShell {
+        nativeBuildInputs = defaultPkgs;
+      };
+      devShells."${macSystem}".default = pkgs.mkShell {
         nativeBuildInputs = defaultPkgs;
       };
     };
