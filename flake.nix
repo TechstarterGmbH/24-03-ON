f8dd4527d8a63296b10506bd61480ee1240e48f3
Fabio Keller <fabio.keller@exb.de>
2024-03-10 15:25:49 +0100

    fix(flake): macos system config

--- flake.nix
+++ flake.nix
@@ -6,14 +6,26 @@
       defaultSystem = "x86_64-linux";
       macSystem = "aarch64-darwin";
       pkgs = import nixpkgs { system = defaultSystem; };
+      pkgsMac = import nixpkgs { system = macSystem; };
 
       markdownToHtmlCli = import ./nix/markdown2html-converter.nix { inherit pkgs; };
+      markdownToHtmlCliMac = import ./nix/markdown2html-converter.nix { inherit pkgsMac; };
+
       marpCli = import ./nix/marp.nix { inherit pkgs; };
+      marpCliMac = import ./nix/marp.nix { inherit pkgsMac; };
+
       
       defaultPkgs = with pkgs; [
         chromium
         markdownToHtmlCli
         marpCli
+        pre-commit
+      ];
+
+      macPkgs = with pkgsMac; [
+        chromium
+        markdownToHtmlCliMac
+        marpCliMac
         pre-commit
       ];
     in
@@ -22,9 +34,7 @@
         nativeBuildInputs = defaultPkgs;
       };
       devShells."${macSystem}".default = pkgs.mkShell {
-        nativeBuildInputs = defaultPkgs;
+        nativeBuildInputs = macPkgs;
       };
     };
 }
-
-
