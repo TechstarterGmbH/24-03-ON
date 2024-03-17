0c6e82ae05c9d8a8ef6c99b6d7596f9d26b8a891
Fabio Keller <fabio.keller@exb.de>
2024-03-17 18:04:28 +0100

    refactor(flake): remove mac support

--- flake.nix
+++ flake.nix
@@ -4,16 +4,11 @@
   outputs = { self, nixpkgs }:
     let
       defaultSystem = "x86_64-linux";
-      macSystem = "aarch64-darwin";
       pkgs = import nixpkgs { system = defaultSystem; };
-      pkgsMac = import nixpkgs { system = macSystem; };
 
       markdownToHtmlCli = import ./nix/markdown2html-converter.nix { inherit pkgs; };
-      markdownToHtmlCliMac = import ./nix/markdown2html-converter.nix { inherit pkgsMac; };
 
       marpCli = import ./nix/marp.nix { inherit pkgs; };
-      marpCliMac = import ./nix/marp.nix { inherit pkgsMac; };
-
       
       defaultPkgs = with pkgs; [
         chromium
@@ -22,18 +17,10 @@
         pre-commit
       ];
 
-      macPkgs = with pkgsMac; [
-        markdownToHtmlCliMac
-        marpCliMac
-        pre-commit
-      ];
     in
     {
       devShells."${defaultSystem}".default = pkgs.mkShell {
         nativeBuildInputs = defaultPkgs;
-      };
-      devShells."${macSystem}".default = pkgs.mkShell {
-        nativeBuildInputs = macPkgs;
       };
     };
 }
