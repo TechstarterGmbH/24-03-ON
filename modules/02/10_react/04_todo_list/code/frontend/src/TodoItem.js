2bc85b8e7dac03fabbf6adcf258cc7ab9f8450fc
Fabio Keller <fabio.keller@exb.de>
2024-07-27 15:36:45 +0200

    feat(02/10/04): day 2

--- modules/02/10_react/04_todo_list/code/frontend/src/TodoItem.js
+++ modules/02/10_react/04_todo_list/code/frontend/src/TodoItem.js
@@ -1,11 +1,46 @@
+import { useState } from "react";
+
 import "./TodoItem.css";
 
-function TodoItem({ todo, onToggleDone }) {
+function TodoItem({ todo, onToggleDone, onDelete, onUpdate }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [updatedText, setUpdatedText] = useState(todo.text);
+
   const todoStrikeClass = todo.isComplete ? 'todo-strikethrough' : '';
+  const updateButtonText = isEditing ? 'Save' : 'Update';
+
+  function handleUpdate() {
+    if (isEditing) {
+      setIsEditing(false);
+
+      const updatedTodo = {
+        ...todo,
+        text: updatedText
+      };
+
+      onUpdate(updatedTodo);
+
+    } else {
+      setIsEditing(true);
+    }
+  }
+
   return (
     <div className="todo">
       <input type="checkbox" checked={todo.isComplete} onChange={onToggleDone} />
-      <span className={todoStrikeClass}>{todo.text}</span>
+      {isEditing ? (
+        <input type="text" value={updatedText} onChange={(e) => setUpdatedText(e.target.value)} />
+      ) : (
+        <span className={todoStrikeClass}>{todo.text}</span>
+      )}
+
+      <div className="todo-btns">
+        <button onClick={handleUpdate}>{updateButtonText}</button>
+        <button onClick={onDelete}>Delete</button>
+      </div>
+
     </div>
   )
 }
--- modules/02/10_react/04_todo_list/code/frontend/src/TodoList.js
+++ modules/02/10_react/04_todo_list/code/frontend/src/TodoList.js
@@ -1,17 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import TodoItem from './TodoItem';
 import TodoInput from './TodoInput';
 
-const defaultTodos = [
-  { id: 1, text: 'Einkaufen gehen', isComplete: false },
-  { id: 2, text: 'Waschen', isComplete: false },
-  { id: 3, text: 'React lernen', isComplete: false },
-];
-
 function TodoList() {
 
-  const [todos, setTodos] = useState(defaultTodos);
+  useEffect(() => {
+    // Hier werden die Daten vom Server geladen
+
+    setTimeout(() => {
+      setTodos([
+        { id: 0, text: 'Learn React', isComplete: false },
+        { id: 1, text: 'Learn Vue', isComplete: false },
+        { id: 2, text: 'Learn Angular', isComplete: false },
+      ]);
+      setLoading(false);
+    }, 2000);
+  });
+
+  const [todos, setTodos] = useState([]);
+
+  const [loading, setLoading] = useState(true);
 
   function todoToggleDone(index) {
     const newTodos = todos.slice();
@@ -25,26 +34,49 @@ function TodoList() {
     setTodos([...todos, todo]);
   }
 
+  function deleteTodo(index) {
+    const newTodos = todos.slice();
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  function updateTodo(updatedTodo) {
+    const newTodos = todos.map(todo => {
+      if (todo.id === updatedTodo.id) {
+        return updatedTodo;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="todo-list">
 
-      <h2>Add new Todo</h2>
+    <>
+      {loading ? <h1>Loading</h1> : (
+      <div className="todo-list">
 
-      <TodoInput addTodo={addTodo} />
+        <h2>Add new Todo</h2>
 
-      <h2>Todo List</h2>
+        <TodoInput addTodo={addTodo} />
 
-      <div className="todos">
-        {todos.map( (todo, index) => (
-          <TodoItem
-            key={index}
-            todo={todo}
-            onToggleDone={() => todoToggleDone(index)}
-          />
-        ))}
+        <h2>Todo List</h2>
+
+        <div className="todos">
+          {todos.map( (todo, index) => (
+            <TodoItem
+              key={index}
+              todo={todo}
+              onToggleDone={() => todoToggleDone(index)}
+              onDelete={() => deleteTodo(index)}
+              onUpdate={updateTodo}
+            />
+          ))}
+        </div>
+
       </div>
-
-    </div>
+      )}
+    </>
   )
 }
 
